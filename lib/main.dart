c42c3c39a7470cc93306cd59ec578a8f657ad070
Alejandro Mira <alejandro.mira7@gmail.com>
2025-04-25 12:27:33 -0500

    config bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'package:push_notification/config/router/app_router.dart';
 import 'package:push_notification/config/theme/app_theme.dart';
+import 'package:push_notification/presentation/blocs/notifications/notifications_bloc.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(
+    MultiBlocProvider(
+      providers: [
+        BlocProvider(create: (_) => NotificationsBloc())
+      ],
+      child: const MainApp()
+    )
+  );
 }
 
 class MainApp extends StatelessWidget {
