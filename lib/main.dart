fa3b25ee6a904b74180eca90d39b33389be2841f
Alejandro Mira <alejandro.mira7@gmail.com>
2025-05-29 15:56:25 -0500

    notifications background

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -10,6 +11,7 @@ import 'package:push_notification/presentation/blocs/notifications/notifications
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  FirebaseMessaging.onBackgroundMessage(firebaseMessagingBackgroundHandler);
   runApp(
     MultiBlocProvider(
       providers: [
--- lib/presentation/blocs/notifications/notifications_bloc.dart
+++ lib/presentation/blocs/notifications/notifications_bloc.dart
@@ -1,9 +1,19 @@
+import 'dart:io';
+
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
+
+import 'package:push_notification/domain/entities/push_message.dart';
 
 part 'notifications_event.dart';
 part 'notifications_state.dart';
+
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  await Firebase.initializeApp();
+  print("pruebaaa::: ${message.messageId}");
+}
 
 // se maneja la logica de negocio -- escucha eventos y emite estados
 class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
@@ -13,6 +23,8 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
   NotificationsBloc() : super(const NotificationsState()) {
 
     on<NotificationStatusChanged>(_notificationStatusChanged);
+
+    on<NotificationReceived>(_onPushMessageReceived);
 
     // verificar estado de las notificaciones
     _initialStatusCheck();
@@ -44,6 +56,14 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
     _getToken();
   }
 
+  void _onPushMessageReceived(NotificationReceived event, Emitter<NotificationsState> emit) {
+    emit(
+      state.copyWith(
+        notifications: [ event.pushMessage, ... state.notifications ]
+      )
+    );
+  }
+
   void _initialStatusCheck() async {
     final settings = await messaging.getNotificationSettings();
     add(NotificationStatusChanged(settings.authorizationStatus));
@@ -57,12 +77,30 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
   }
 
   void _handleRemoteMessage(RemoteMessage message) {
-    print('Message data: ${message.data}');
     if(message.notification == null) return;
-    print('Message notification: ${message.notification}');
+    final notification = PushMessage(
+      messageId: message.messageId
+        ?.replaceAll(':', '').replaceAll('%', '')
+        ?? '',
+      title: message.notification!.title ?? '',
+      body: message.notification!.body ?? '',
+      sentDate: message.sentTime ?? DateTime.now(),
+      data: message.data,
+      imageUrl: Platform.isAndroid
+        ? message.notification!.android?.imageUrl
+        : message.notification!.apple?.imageUrl
+    );
+
+    add(NotificationReceived(notification));
   }
 
   void _onForegroundMessage() {
     FirebaseMessaging.onMessage.listen(_handleRemoteMessage);
+  }
+
+  PushMessage? getMessageById(String pushMessageId) {
+    final exist = state.notifications.any((element) => element.messageId == pushMessageId);
+    if(!exist) return null;
+    return state.notifications.firstWhere((element) => element.messageId == pushMessageId);
   }
 }
